27e56fa005fc8412fac71278b45ce40f97713356
PabloDelgadoSoto <pablodelsot@gmail.com>
2024-10-07 22:09:23 +0200

    completo

--- ejercicio.js
+++ ejercicio.js
@@ -1,11 +1,12 @@
 let table;
 let cabeza;
-var tablero = 10;
-var serpiente = ['5-4', '5-3', '5-2'];
+var fruta = [];
 
-var verde = 'background-color:green';
-var rojo = 'background-color:red';
-var azul = 'background-color:blue';
+var comida = false;
+
+var colorSerpiente = 'background-color:green';
+var colorFruta = 'background-color:red';
+var colorFondo = 'background-color:blue';
 
 const movimientos ={
     'ArrowUp':[0,-1],
@@ -13,8 +14,12 @@ const movimientos ={
     'ArrowLeft':[1,-1],
     'ArrowRight':[1,1]
 }
-
 const opciones = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+
+const tablero = 50;
+const turnos = tablero/2;
+var contFruta = tablero/2;
+var serpiente = [];
 
 window.onload = function () {
     table = document.getElementById('table');
@@ -30,9 +35,9 @@ const hacerTabla = function () {
             tr.appendChild(td);
         }
         table.appendChild(tr);
+        serpiente = [tablero/2+'-'+tablero/2, tablero/2+'-'+(tablero/2-1), tablero/2+'-'+(tablero/2-2)];
     }
-    serpiente.push(tablero/2+'-'+tablero/2);
-    pintarSerpiente(verde);
+    pintarSerpiente(colorSerpiente);
     document.addEventListener('keydown', function(event){
         inArray(opciones, event.key)?moverCabeza(event.key):'';
     })
@@ -44,10 +49,18 @@ const moverCabeza = function(tecla){
     marca[0] = desbordar(marca[0]);
     marca[1] = desbordar(marca[1]);
     let suma = marca[0]+'-'+marca[1];
-    pintarSerpiente(azul);
-    serpiente[0] = suma;
+    pintarSerpiente(colorFondo);
     moverCuerpo();
-    pintarSerpiente(verde);
+    serpiente[0] = suma;
+    comer(serpiente[0]);
+    if(contFruta == turnos){
+        crearFruta();
+    }
+    pintarSerpiente(colorSerpiente);
+    contFruta++;
+    if(perder(serpiente, serpiente[0])){
+        rehacer();
+    }
 }
 
 const moverCuerpo = function(){
@@ -59,6 +72,10 @@ const moverCuerpo = function(){
     for(let i = 1; i < serpiente.length; i++){
         serpiente[i] = guardar[i-1];
     }
+    if(comida){
+        serpiente.push(guardar[serpiente.length-1]);
+        comida = false;
+    }
 }
 
 const pintarSerpiente = function(color){
@@ -69,11 +86,28 @@ const pintarSerpiente = function(color){
     
 }
 
+const crearFruta = function(){
+    let id = Math.floor(Math.random()*tablero)+'-'+Math.floor(Math.random()*tablero);
+    if(inArray(serpiente, id)){
+        crearFruta();
+    }
+    let f = document.getElementById(id);
+    f.setAttribute('style', colorFruta);
+    fruta.push(id);
+    contFruta = 0;
+}
+
+const comer = function(cabeza){
+    if(inArray(fruta, cabeza)){
+        comida = true;
+    }
+}
+
 const desbordar = function (num) {
     if (num < 0) {
-        return 0;
-    } else if (num > tablero) {
         return tablero;
+    } else if (num > tablero) {
+        return 0;
     }
     return num;
 };
@@ -85,3 +119,16 @@ const inArray = function (arr, e) {
     return false;
 };
 
+const perder = function(arr, e){
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] == e) return true;
+    }
+    return false;
+}
+
+const rehacer = function(){
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
+    }
+    document.write('Perdiste, recarga la página para volver a comenzar');
+}
